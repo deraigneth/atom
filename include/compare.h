36ff131fc0d9be5eb770879a46e86d1725b10d1a
Tomadoke <doquetthom@eisti.eu>
2019-06-10 10:36:59 +0200

    commentaire ajoutée

--- include/compare.h
+++ include/compare.h
@@ -15,7 +15,10 @@
 #define TOLERANCE 1e-15
 
 double compare(double *a,double *b,int num,std::string legend,double tol);
+/* fonction qui compare tous les éléments de la liste et qui vérifie que chaque différence en valeur absolue entre deux éléments de deux listes est inférieure à la tolérance fixée, elle affiche ensuite un message.
+ * Si de plus t=t/num, elle affiche un autre message*/
 
 int comd(double a,double b);
+/*fonction qui vérifie si deux flottant sont différent ou pas*/
 
 #endif /* COMPARE_H_ */
